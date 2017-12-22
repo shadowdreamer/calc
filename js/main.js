f0c548292306587eeb56219a4ac9947cf6731603
shadowdreamer <uzumaki@yeah.net>
2017-12-22 08:02:11 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,40 +1,60 @@
 $(document).ready(function(){
 	$("#num1").click(function(){
-		$("#input span:last").text($("#input span:last").text()+1)   /*数字0~9*/
+		$("#shizi span:last").text($("#shizi span:last").text()+1)   /*数字0~9*/
 	});	
 	$("#num2").click(function(){
-		$("#input span:last").text($("#input span:last").text()+2)
+		$("#shizi span:last").text($("#shizi span:last").text()+2)
 	});
 	$("#num3").click(function(){
-		$("#input span:last").text($("#input span:last").text()+3)
+		$("#shizi span:last").text($("#shizi span:last").text()+3)
 	});	
 	$("#num4").click(function(){
-		$("#input span:last").text($("#input span:last").text()+4)
+		$("#shizi span:last").text($("#shizi span:last").text()+4)
 	});	
 	$("#num5").click(function(){
-		$("#input span:last").text($("#input span:last").text()+5)
+		$("#shizi span:last").text($("#shizi span:last").text()+5)
 	});
 	$("#num6").click(function(){
-		$("#input span:last").text($("#input span:last").text()+6)
+		$("#shizi span:last").text($("#shizi span:last").text()+6)
 	});	
 	$("#num7").click(function(){
-		$("#input span:last").text($("#input span:last").text()+7)
+		$("#shizi span:last").text($("#shizi span:last").text()+7)
 	});
 	$("#num8").click(function(){
-		$("#input span:last").text($("#input span:last").text()+8)
+		$("#shizi span:last").text($("#shizi span:last").text()+8)
 	});	
 	$("#num9").click(function(){
-		$("#input span:last").text($("#input span:last").text()+9)
+		$("#shizi span:last").text($("#shizi span:last").text()+9)
 	});	
 	$("#num0").click(function(){
-		$("#input span:last").text($("#input span:last").text()+0)
+		$("#shizi span:last").text($("#shizi span:last").text()+0)
 	});	
 	$("#dot").click(function(){
-		$("#input span:last").text($("#input span:last").text()+".")
+		$("#shizi span:last").text($("#shizi span:last").text()+".")
 	});	
 	
 	$("#jia").click(function(){
-		$("#input span:last").after("+"+"<span></span>")
+		$("#shizi span:last").after("<span>+</span>"+"<span></span>")
+	});
+	$("#jian").click(function(){
+		$("#shizi span:last").after("<span>-</span>"+"<span></span>")
+	});
+	
+	$("#chen").click(function(){
+		$("#shizi span:last").after("<span>*</span>"+"<span></span>")
+	});
+	
+	$("#chu").click(function(){
+		$("#shizi span:last").after("<span>/</span>"+"<span></span>")
+	});	
+
+	$("#dengyu").click(function(){
+		$("#jieguo").text(eval($("#shizi span").text()))
+	});
+	
+	$("#allclear").click(function(){
+		$("#jieguo").text("");
+		$("#shizi").html("<span></span>");
 	});
 	
 
