65b047034d4ea86058b2a75aefe5a4ce09c5afc5
shadowdreamer <uzumaki@yeah.net>
2017-12-22 21:28:50 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,76 +1,18 @@
-var ANS;
-var ce = new CalcEval();//CalcEval引擎是一个专门解决javascript浮点数误差的的引擎，能够完美的解决各种复合的运算，最终输出正确的结果。创建引擎对象
-$(document).ready(function(){
-	$("#num1").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+1)   /*数字0~9*/
-	});	
-	$("#num2").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+2)
-	});
-	$("#num3").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+3)
-	});	
-	$("#num4").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+4)
-	});	
-	$("#num5").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+5)
-	});
-	$("#num6").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+6)
-	});	
-	$("#num7").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+7)
-	});
-	$("#num8").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+8)
-	});	
-	$("#num9").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+9)
-	});	
-	$("#num0").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+0)
-	});	
-	$("#dot").click(function(){
-		$("#shizi span:last").text($("#shizi span:last").text()+".")
-	});	
-	
-	$("#jia").click(function(){
-		$("#shizi span:last").after("<span>+</span>"+"<span></span>")
-	});
-	$("#jian").click(function(){
-		$("#shizi span:last").after("<span>-</span>"+"<span></span>")
-	});
-	
-	$("#chen").click(function(){
-		$("#shizi span:last").after("<span>*</span>"+"<span></span>")
-	});
-	
-	$("#chu").click(function(){
-		$("#shizi span:last").after("<span>/</span>"+"<span></span>")
-	});	
-
-	$("#dengyu").click(function(){
-		$("#jieguo").text(ce.eval($("#shizi span").text()));
-		ANS = ce.eval($("#jieguo").text());
-	});
-	
-	$("#allclear").click(function(){
-		$("#jieguo").text("");
-		$("#shizi").html("<span></span>");
-	});
-	
-	$("#ans").click(function(){		
-		$("#shizi span:last").text($("#shizi span:last").text()+"ANS");
-	});
-	
-
-	
-});
-
-
-
-
-
-
-
+	var ce = new CalcEval();
+	function inputwrite(n){
+		function writethings(x){
+			$("#shizi span:last").text($("#shizi span:last").text()+x);
+		}
+		switch (n) {
+			case 0 : case  1 : case  2 : case  3 : case  4 : case  5 : case 6 : case  7 : case 8 : case 9 : writethings(n);break;
+			case "dot" : writethings(".");break;
+			case "chen" : writethings("*");break;
+			case "chu" : writethings("/");break;
+			case "jia" : writethings("+");break;
+			case "jian" : writethings("-");break;
+			case "zuokuohao" : writethings("(");break;
+			case "youkuohao" : writethings(")");break;
+			case "AC" : $("#shizi").html("<span></span>"); $("#jieguo").html(""); break;
+			case "dengyu" : $("#jieguo").text(ce.eval($("#shizi span").text())); break;
+		}
+	}	
