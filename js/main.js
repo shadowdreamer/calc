c12d1e1a19c26c767698a12bec5c9220a6a088f1
shadowdreamer <uzumaki@yeah.net>
2017-12-24 11:51:39 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,8 +27,8 @@
 			case "back" : crrtEdit.text(temp.substring(0,temp.length - 1));break;
 		}
 		changed = $("#shizi").text().replace(/\*/g,"×");
-		changed = change.replace(/\//g,"÷");
-		changed = change.replace(/\(pi\)/g,"π");		
+		changed = changed.replace(/\//g,"÷");
+		changed = changed.replace(/\(pi\)/g,"π");		
 		$("#showshizi").text(changed);
 		
 	};		
